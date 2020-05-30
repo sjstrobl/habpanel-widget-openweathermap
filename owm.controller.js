1664c62313898897292b607cb214cb065631f197
sjstrobl <64354123+sjstrobl@users.noreply.github.com>
2020-05-31 00:36:38 +0200

    owm.controller.js: use owm forecast by day not hour

--- owm.controller.js
+++ owm.controller.js
@@ -8,32 +8,21 @@
 				function loadOwmForecast() {
 					console.log("Processing OWM widget forecast");
 					try {
-						var today = new Date();
-						var hoursToNoon = (24 - today.getHours()) + 12;
-						for (var day = 0; day < 4; day++) {
-							setForecastItems(day, getClosest(hoursToNoon + (day * 24)));
-						}
+					    setForecastItems(0);
+					    setForecastItems(1);
+					    setForecastItems(2);
+					    setForecastItems(3);
 					} catch (err) {
 						console.log("Error during OWM widget: " + err)
 					}
                                 }
 
 				// Set forecast values for particular day
-				function setForecastItems(day, hour) {
-					$scope["temp" + day] = $scope.itemState('Weather_OWM_Temp_h' + hour);
-					$scope["condition" + day] = $scope.itemState('Weather_OWM_Condition_h' + hour);
-					$scope["condition_id" + day] = $scope.itemState('Weather_OWM_ConditionId_h' + hour);
-					$scope["date_time" + day] = $scope.itemState('Weather_OWM_DateTime_h' + hour);
-				}
-
-				// Find closest to value in array
-				function getClosest(goal) {
-					var hours = Array.from(Array(32).keys(), (h) => (h + 1) * 3);  // Generate array with stepsize 3 until 96
-					var closest = hours.reduce(function (prev, curr) {
-						return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
-					});
-
-					return closest
+				function setForecastItems(day) {
+					$scope["temp" + day] = $scope.itemState('Weather_OWM_Temp_d' + day);
+					$scope["condition" + day] = $scope.itemState('Weather_OWM_Condition_d' + day);
+					$scope["condition_id" + day] = $scope.itemState('Weather_OWM_ConditionId_d' + day);
+					$scope["date_time" + day] = $scope.itemState('Weather_OWM_DateTime_d' + day);
 				}
 
 				// Run function when an items updates
